1b59894f4770a931107037f61636b9916026bbd0
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-16 02:43:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,7 +28,8 @@ def now_serving(katz_deli)
   if katz_deli.size > 0
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
-  elsif katz_deli.size == 0
+  else katz_deli.size == 0
+    puts 
   end
   
   
