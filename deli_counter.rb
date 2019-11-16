18caf1413c322c53846918e350cf97a43ae858d3
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-16 02:46:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(array)
     new_array<< (" #{count}. #{name }")
     count += 1
   end
-  puts "The line is currently:#{new_array.join("")}"
+   puts "The line is currently:#{new_array.join("")}"
  else puts "The line is currently empty"
  end
 end
@@ -20,8 +20,7 @@ end
 
 def take_a_number(katz_deli, name)
      katz_deli<< name
-     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
@@ -29,10 +28,7 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   else katz_deli.size == 0
-    puts "
+    puts "There is nobody waiting to be served!"
   end
-  
-  
 end
 
-"
