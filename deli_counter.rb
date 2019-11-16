d450cf7da4b8da3092853cd1704190c4a3fc314e
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-16 00:30:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+
+
+katz_deli = []
+
+def line(array)
+ new_array = []
+   count = 1
+ if array.length > 1 
+    array.each do |name|
+    new_array<< (" #{count}. #{name }")
+    count += 1
+  end
+  puts "The line is currently:#{new_array.join("")}"
+ else puts "The line is currently empty."
+ end
+end
+
+
+def take_a_number(new_array, name)
+  count = 1
+  new2_array = []
+  if new_array.length > 1 
+     new_array.each do |name| 
+     new_array2<< (" #{count}. #{name } ") || (" #{count}. #{new_name}") 
+     count += 1
+   end
+  else  puts "Welcome, #{name}. You are number #{count} in line."
+  end
+end
+=begin
+def now_serving
+  
+end
+=end
+
